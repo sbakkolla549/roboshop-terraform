173a4b56f2cd4c5bbf6d6227e95e573f80d4217c
Srikanth Bakkolla <sbakkolla@gmail.com>
2024-11-12 12:50:27 +0530

    Add files via upload

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,10 @@
+variable "env" {}
+variable "component_name" {}
+variable "instance_type" {}
+variable "app_port" {}
+variable "zone_id" {}
+variable "domain_name" {}
+variable "vault_token" {}
+variable "volume_size" {}
+
+
