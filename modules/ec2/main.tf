6b04b565c4ef3a7beff95df419c3266f786c0bd2
srikanthb549 <sbakkolla@gmail.com>
2024-11-12 14:20:45 +0530

    my commit

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -59,6 +59,7 @@ resource "null_resource" "ansible-pull" {
     }
 
     inline = [
+      "sudo labauto ansible",
       "ansible-pull -i localhost, -U https://github.com/sbakkolla549/roboshop-ansible roboshop.yml -e env=${var.env} -e component=${var.component_name} -e vault_token=${var.vault_token}"
     ]
   }
